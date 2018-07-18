660885a8d695d372663d9ada22ba07c55115a3b3
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-07-19 11:19:44 +1200

    PlaceTestPoints places point in a cirle + added back the icons

--- Assets/PlaceTestPoints.cs
+++ Assets/PlaceTestPoints.cs
@@ -5,27 +5,31 @@ using UnityEngine;
 public class PlaceTestPoints : MonoBehaviour {
 	public Transform center;
 	public Transform leftFront, rightFront, leftBack, rightBack, left, right, front, back;
-	public float distanceTocenterX;
-	public float distanceTocenterZ; 
+	public float distanceToInscribedSquare;
+	// public float distanceTocenterZ; 
+	[SerializeField]
+	private float radius;
 
 	// Use this for initialization
 	void Start () {
-		leftFront.position = center.position + new Vector3(-distanceTocenterX, 0f, distanceTocenterZ);
+		leftFront.position = center.position + new Vector3(-distanceToInscribedSquare, 0f, distanceToInscribedSquare);
 		leftFront.rotation = Quaternion.Euler(0f, -45f, 0f);
-		rightFront.position = center.position + new Vector3(distanceTocenterX, 0f, distanceTocenterZ);
+		rightFront.position = center.position + new Vector3(distanceToInscribedSquare, 0f, distanceToInscribedSquare);
 		rightFront.rotation = Quaternion.Euler(0f, 45f, 0f);		
-		leftBack.position = center.position + new Vector3(-distanceTocenterX, 0f, -distanceTocenterZ);
+		leftBack.position = center.position + new Vector3(-distanceToInscribedSquare, 0f, -distanceToInscribedSquare);
 		leftBack.rotation = Quaternion.Euler(0f, -135f, 0f);
-		rightBack.position = center.position + new Vector3(distanceTocenterX, 0f, -distanceTocenterZ);
+		rightBack.position = center.position + new Vector3(distanceToInscribedSquare, 0f, -distanceToInscribedSquare);
 		rightBack.rotation = Quaternion.Euler(0f, 135f, 0f);
 
-		front.position = center.position + new Vector3(0f, 0f, distanceTocenterZ);
+		radius = Vector3.Distance(center.position, rightBack.position);
+
+		front.position = center.position + new Vector3(0f, 0f, radius);
 		front.localScale *= 2f;
-		back.position = center.position + new Vector3(0f, 0f, -distanceTocenterZ);
+		back.position = center.position + new Vector3(0f, 0f, -radius);
 		back.rotation = Quaternion.Euler(0f, 180f, 0f);	
-		left.position = center.position + new Vector3(-distanceTocenterX, 0f, 0f);		
+		left.position = center.position + new Vector3(-radius, 0f, 0f);		
 		left.rotation = Quaternion.Euler(0f, -90f, 0f);
-		right.position = center.position + new Vector3(distanceTocenterX, 0f, 0f);	
+		right.position = center.position + new Vector3(radius, 0f, 0f);	
 		right.rotation = Quaternion.Euler(0f, 90f, 0f);		
 	}
 }
