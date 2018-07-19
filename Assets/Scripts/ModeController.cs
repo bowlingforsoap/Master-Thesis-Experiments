f1efb488152e5c84fa63f53791f86d1d2c26e43e
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-07-19 14:32:47 +1200

    Tutorial and Experiment modes & adjusted the markings on GuessCylinder

---
+++ Assets/Scripts/ModeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeController : MonoBehaviour {
+
+	public GameObject virtualBuilding;
+	// public GameObject campus;
+
+	// Use this for initialization
+	void Start () {
+		// GoInTutorialMode();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoInTutorialMode() {
+		virtualBuilding.SetActive(true);
+		// campus.SetActive(true);
+	}
+
+	public void GoInExperimentMode() {
+		virtualBuilding.SetActive(false);
+		// campus.SetActive(false);
+	}
+
+}
---
+++ Assets/Scripts/ModelControllerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ModeController))]
+public class ModelControllerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+
+		ModeController modeController = (ModeController) target;
+		if (GUILayout.Button("Tutorial Mode")) {
+			modeController.GoInTutorialMode();
+		}
+		if (GUILayout.Button("Experiment Mode")) {
+			modeController.GoInExperimentMode();
+		}
+	}
+}
