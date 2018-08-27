eba8b55ea02be671b81428fe0725d7566f7f910a
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-08-27 14:55:26 +0200

    removed debug logs

--- Assets/Scripts/ChangeBuildingsColor.cs
+++ Assets/Scripts/ChangeBuildingsColor.cs
@@ -11,14 +11,8 @@ public class ChangeBuildingsColor : MonoBehaviour {
 	void Start () {
 		// Setup materials for each color
 		materials = new Material[colors.Length];
-		// Debug
-		Dictionary<int, int> colorCounts = new Dictionary<int, int>();
 
 		for (int i = 0; i < colors.Length; i++) {
-			// Debug
-			Debug.Log(colors[i]);
-			colorCounts.Add(i, 0);
-
 			Material temp = new Material(shader);
 			temp.color = colors[i];
 			materials[i] = temp;
@@ -37,14 +31,6 @@ public class ChangeBuildingsColor : MonoBehaviour {
 
 			int randColorIndex = Random.Range(0, materials.Length);
 			child.GetComponent<MeshRenderer>().material = materials[randColorIndex];
-
-			//Debug
-			colorCounts[randColorIndex]++;
-		}
-
-		//Debug
-		foreach (var colorCount in colorCounts) {
-			Debug.Log(colorCount.Key + ": " + colorCount.Value);
 		}
 	}
 	
